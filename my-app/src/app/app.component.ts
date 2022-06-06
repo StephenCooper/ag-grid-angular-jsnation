1c82e1c38bb9d699fe12eaeb01f08bf49efa66ff
Stephen Cooper <scooperdev@gmail.com>
2022-06-06 10:23:47 +0100

    Starting point for grid properties

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
   template: `
-    <p> App Works!</p>
+    <ag-grid-angular
+    style="width: 100%; height: 100%"
+    class="ag-theme-alpine"
+      [rowData]="rowData"
+      [columnDefs]="columnDefs"
+    >
+    </ag-grid-angular>
   `,
-  styles : []
+  styles: []
 })
 export class AppComponent {
-  title = 'my-app';
+  public rowData = [
+    { "athlete": "Michael Phelps", "age": 23, "country": "United States", "year": 2008, "date": "24/08/2008", "sport": "Swimming", "gold": 8, "silver": 0, "bronze": 0, "total": 8 },
+    { "athlete": "Libby Lenton-Trickett", "age": 23, "country": "Australia", "year": 2008, "date": "24/08/2008", "sport": "Swimming", "gold": 2, "silver": 1, "bronze": 1, "total": 4 },
+    { "athlete": "Shawn Johnson", "age": 16, "country": "United States", "year": 2008, "date": "24/08/2008", "sport": "Gymnastics", "gold": 1, "silver": 3, "bronze": 0, "total": 4 }
+  ];
+  public columnDefs: ColDef[] = [
+    { field: 'athlete' },
+    { field: 'age' },
+    { field: 'country' },
+    { field: 'year' },
+    { field: 'date' },
+    { field: 'sport' },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+    { field: 'total' }
+  ];
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
